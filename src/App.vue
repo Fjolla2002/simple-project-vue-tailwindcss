862ebd00766e3808f436edc8c444bd3bf171822b
Fjolla Selimi <84503129+Fjolla2002@users.noreply.github.com>
2024-04-25 14:36:48 +0200

    added icons and navbar

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,13 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
+import Navbar from "./components/Navbar.vue";
+
 export default {
   components: {
-    Navbar
-  }
-}
+    Navbar,
+  },
+};
 </script>
 
 <style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="w-full mx-auto bg-primaryWhite py-6">
     <div class="w-11/12 lg:w-3/5 mx-auto flex items-center justify-center">
+        <font-awesome-icon icon="bars" class="text-2xl"/>
     </div>
   </div>
 </template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars)
+
+export default {
+  components: {
+    FontAwesomeIcon
+  }
+}
+</script>
 
 
