bf54cb7599c080916ee090c421bd045923ab0c98
Fjolla Selimi <84503129+Fjolla2002@users.noreply.github.com>
2024-04-26 09:36:34 +0200

    formatted code on home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,24 @@
 <template>
-  <div>
-    <hero />
-    <elevate />
-    <elegance />
-    <strategy />
-    <statistics />
-    <services />
-    <products />
-  </div>
+  <hero></hero>
+  <elevate></elevate>
+  <elegance></elegance>
+  <strategy></strategy>
+  <statistics></statistics>
+  <services></services>
+  <products></products>
 </template>
 
 <script>
-import Hero from '../components/Hero.vue';
-import Elevate from '../components/Elevate.vue';
-import Elegance from '../components/Elegance.vue';
-import Strategy from '../components/Strategy.vue';
-import Statistics from '../components/Statistics.vue';
-import Services from '../components/Services.vue';
-import Products from '../components/Products.vue';
+import Hero from "../components/Hero.vue";
+import Elevate from "../components/Elevate.vue";
+import Elegance from "../components/Elegance.vue";
+import Strategy from "../components/Strategy.vue";
+import Statistics from "../components/Statistics.vue";
+import Services from "../components/Services.vue";
+import Products from "../components/Products.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     Hero,
     Elevate,
@@ -28,7 +26,7 @@ export default {
     Strategy,
     Statistics,
     Services,
-    Products
-  }
-}
+    Products,
+  },
+};
 </script>
