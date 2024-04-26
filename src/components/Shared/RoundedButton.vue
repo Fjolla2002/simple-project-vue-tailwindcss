1860d7f1aea8633cab3de78af5f9aa81370b82aa
Fjolla Selimi <84503129+Fjolla2002@users.noreply.github.com>
2024-04-26 10:29:00 +0200

    fiexed a bug in roundedButton

--- src/components/Shared/RoundedButton.vue
+++ src/components/Shared/RoundedButton.vue
@@ -1,25 +1,29 @@
 <template>
-    <button
-      class="mt-12 lg:px-14 px-8 py-4 rounded-full tracking-wider text-base border-none uppercase font-bold font-openSans transition-all duration-300 ease-in-out"
-      :class="`text-${color} bg-${background} hover:bg-${hoverBackground}`"
-    >
-      <slot></slot>
-    </button>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      color: {
-        type: String,
-      },
-      background: {
-        type: String,
-      },
-      hoverBackground: {
-        type: String,
-      },
+  <button
+    class="mt-12 lg:px-14 px-8 py-4 rounded-full tracking-wider text-base border-none uppercase font-bold font-openSans transition-all duration-300 ease-in-out"
+    :class="style"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
     },
-  };
-  </script>
-  
+    background: {
+      type: String,
+    },
+    hoverBackground: {
+      type: String,
+    },
+  },
+  computed: {
+    style() {
+      return `text-${this.color} bg-${this.background} hover:bg-${this.hoverBackground}`
+    }
+  }
+};
+</script>
