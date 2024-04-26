779103adfe818a98f7fc4bb994cbb68270eb629f
Fjolla Selimi <84503129+Fjolla2002@users.noreply.github.com>
2024-04-26 14:48:44 +0200

    added styling to navbar buttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="w-full mx-auto bg-primaryWhite py-6">
     <div class="w-11/12 lg:w-3/5 mx-auto flex items-center justify-center">
-      <font-awesome-icon icon="bars" class="text-2xl cursor-pointer" @click="toggleMenu" />
+      <font-awesome-icon
+        icon="bars"
+        class="text-2xl cursor-pointer text-primaryBlack hover:text-primaryGray transition-all duration-300 ease-in-out"
+        @click="toggleMenu"
+      />
     </div>
     <div
       class="w-full min-h-screen absolute top-0 left-0 z-50 overflow-hidden bg-black/[.7]"
-      :class="navbarHidden"
+      :class="{ hidden: !navbarVisible }"
     >
-      <span class="absolute top-0 left-0 w-full h-full bg-transparent" @click="toggleMenu"></span>
+      <span
+        class="absolute top-0 left-0 w-full h-full bg-transparent"
+        @click="toggleMenu"
+      ></span>
       <div
-        class="absolute h-full lg:w-1/6 w-1/2 bg-secondaryBlack text-primaryWhite"
+        class="absolute h-full lg:w-1/6 w-1/2 bg-secondaryBlack text-primaryWhite transition-all duration-300 ease-in-out"
       >
         <div class="relative flex flex-col items-center p-8">
-          <font-awesome-icon icon="x" class="absolute top-0 right-0 p-8 text-2xl cursor-pointer" @click="toggleMenu" />
+          <font-awesome-icon
+            icon="x"
+            class="absolute top-0 right-0 p-8 text-2xl cursor-pointer text-primaryWhite hover:text-primaryGray"
+            @click="toggleMenu"
+          />
           <ul class="text-center lg:text-xl text-2xl mt-12">
             <li class="my-2"><a href="/" @click="toggleMenu">Home</a></li>
-            <li class="my-"><a href="/#statistics" @click="toggleMenu">About</a></li>
-            <li class="my-2"><a href="/#contact" @click="toggleMenu">Contact</a></li>
+            <li class="my-">
+              <a href="/#statistics" @click="toggleMenu">About</a>
+            </li>
+            <li class="my-2">
+              <a href="/#contact" @click="toggleMenu">Contact</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -38,13 +53,12 @@ export default {
   data() {
     return {
       navbarVisible: false,
-      navbarHidden: "hidden",
     };
   },
   methods: {
     toggleMenu() {
       this.navbarVisible = !this.navbarVisible;
-      this.navbarHidden = this.navbarVisible ? "" : "hidden";
+      document.body.style.overflow = this.navbarVisible ? "hidden" : "auto";
     },
   },
 };
