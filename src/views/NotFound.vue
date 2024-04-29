1c6a13bd006887c65d9c0f86f82d8233797ca23f
Fjolla Selimi <84503129+Fjolla2002@users.noreply.github.com>
2024-04-29 15:18:46 +0200

    added notfound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,24 +1,29 @@
 <template>
-    <div class="min-h-screen flex flex-col justify-center items-center bg-primaryGray text-primaryWhite">
-        <h1 class="text-5xl text-center">Page Not Found</h1>
-        <font-awesome-icon
-        icon="face-frown"
-        class="text-9xl mt-8"
-      />
-        <router-link to="/" class="mt-12 border-2 border-primaryWhite rounded-3xl px-6 py-2 hover:border-primaryBlack hover:text-primaryBlack transition-all duration-200 ease-in-out">Back to Home</router-link>
-    </div>
+  <div
+    class="min-h-screen flex flex-col justify-center items-center bg-backgroundGray text-primaryBlack"
+  >
+    <section-title>Page Not Found</section-title>
+    <font-awesome-icon icon="face-frown" class="text-9xl mt-8" />
+    <router-link
+      to="/"
+      class="mt-12 border-2 border-primaryBlack rounded-3xl px-6 py-2 hover:bg-secondaryGray hover:scale-105 transition-all duration-200 ease-in-out"
+      >Back to Home</router-link
+    >
+  </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {faFaceFrown } from "@fortawesome/free-solid-svg-icons";
+import { faFaceFrown } from "@fortawesome/free-solid-svg-icons";
+import SectionTitle from "../components/Shared/Title.vue";
 
 library.add(faFaceFrown);
 
 export default {
-    components: {
-        FontAwesomeIcon,
-    }
-}
+  components: {
+    FontAwesomeIcon,
+    SectionTitle,
+  },
+};
 </script>
