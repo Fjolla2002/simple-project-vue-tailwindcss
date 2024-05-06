e62b9c270aea3abb56e216e5a04da0098cf492b1
Fjolla Selimi <84503129+Fjolla2002@users.noreply.github.com>
2024-05-06 11:12:37 +0200

    another change

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,25 @@
 <template>
-  <div
-    class="min-h-screen flex flex-col justify-center items-center bg-primaryWhite text-primaryBlack"
-  >
-    <h2 class="text-9xl font-ubuntu font-bold mb-8">404</h2>
-    <section-title
-      >Page not found <font-awesome-icon icon="face-frown"
-    /></section-title>
-    <subtitle>Oops! The page you are looking for does not exist. </subtitle>
-    <router-link
-      to="/"
-      class="mt-12 border-2 border-primaryBlack rounded-3xl px-6 py-2 hover:scale-105 transition-all duration-200 ease-in-out"
-      >Back to Home</router-link
-    >
+  <div class="bg-primaryWhite text-primaryBlack">
+    <container>
+      <div class="min-h-screen flex flex-col justify-center items-center">
+        <h2 class="text-9xl font-ubuntu font-bold mb-8">404</h2>
+        <div class="text-center">
+          <section-title
+            >Page not found <font-awesome-icon icon="face-frown"
+          /></section-title>
+        </div>
+        <div class="text-center">
+          <subtitle
+            >Oops! The page you are looking for does not exist.</subtitle
+          >
+        </div>
+        <router-link
+          to="/"
+          class="mt-12 border-2 border-primaryBlack rounded-3xl px-6 py-2 hover:scale-105 transition-all duration-200 ease-in-out"
+          >Back to Home</router-link
+        >
+      </div>
+    </container>
   </div>
 </template>
 
@@ -21,6 +29,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faFaceFrown } from "@fortawesome/free-solid-svg-icons";
 import SectionTitle from "../components/Shared/Title.vue";
 import Subtitle from "../components/Shared/Subtitle.vue";
+import Container from "../components/Shared/Container.vue";
 
 library.add(faFaceFrown);
 
@@ -29,6 +38,7 @@ export default {
     FontAwesomeIcon,
     SectionTitle,
     Subtitle,
+    Container,
   },
 };
 </script>
