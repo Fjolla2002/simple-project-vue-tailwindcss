1a1a4190e4b5abbe3527cc3803a9225f3c5d587e
Fjolla Selimi <84503129+Fjolla2002@users.noreply.github.com>
2024-05-08 11:59:33 +0200

    displayed details of each teammate

--- src/assets/data/dummyData.js
+++ src/assets/data/dummyData.js
@@ -8,6 +8,8 @@ import product7 from "../images/product7.jpeg";
 import product8 from "../images/product8.jpg";
 import service1 from "../images/service1.jpeg";
 import service2 from "../images/service2.jpeg";
+import profile1 from "../images/profile.png";
+import profile2 from "../images/profile1.jpg";
 
 const data = {
   products: [
@@ -54,22 +56,26 @@ const data = {
     {
       name: "Olivia Anderson",
       position: "Lead Developer",
-      desc: "Olivia is a seasoned software engineer with a passion for creating innovative and efficient solutions to complex problems."
+      desc: "Olivia is a seasoned software engineer with a passion for creating innovative and efficient solutions to complex problems.",
+      image: profile1
     },
     {
       name: "Ethan Martinez",
       position: "Marketing Manager",
-      desc: "Ethan excels in crafting compelling marketing strategies and campaigns that resonate with our audience, driving brand visibility and engagement."
+      desc: "Ethan excels in crafting compelling marketing strategies and campaigns that resonate with our audience, driving brand visibility and engagement.",
+      image: profile2
     },
     {
       name: "Maya Rodriguez",
       position: "UX/UI Designer",
-      desc: "Maya is a creative and detail-oriented designer, dedicated to crafting seamless user experiences and visually stunning interfaces."
+      desc: "Maya is a creative and detail-oriented designer, dedicated to crafting seamless user experiences and visually stunning interfaces.",
+      image: profile1
     },
     {
       name: "Alexander Carter",
       position: "Operations Manager",
-      desc: "Alexander oversees our day-to-day operations, ensuring smooth-workflow and efficiency across the organization with a focus on continous improvement."
+      desc: "Alexander oversees our day-to-day operations, ensuring smooth-workflow and efficiency across the organization with a focus on continous improvement.",
+      image: profile2
     },
   ]
 };
--- src/components/OurTeam.vue
+++ src/components/OurTeam.vue
@@ -6,6 +6,22 @@
         <div class="w-full text-center lg:w-3/4">
           <subtitle>Get to know the faces behind our success.</subtitle>
         </div>
+        <div
+          class="mt-12 w-full grid grid-cols-1 gap-20 lg:grid-cols-4 md:grid-rows-2 sm:grid-cols-2"
+        >
+          <div
+            v-for="(teammate, index) in team"
+            class="w-full flex flex-col items-center justify-center text-center"
+          >
+            <div
+              class="bg-cover bg-center bg-no-repeat rounded-full w-3/4 h-[240px]"
+              :style="{ backgroundImage: 'url(' + teammate.image + ')' }"
+            ></div>
+            <h4 class="my-4 text-2xl">{{ teammate.name }}</h4>
+            <h6 class="my-2 text-xl">{{ teammate.position }}</h6>
+            <p class="my-4 text-base">{{ teammate.desc }}</p>
+          </div>
+        </div>
       </div>
     </container>
   </div>
@@ -15,6 +31,7 @@
 import SectionTitle from "./Shared/Title.vue";
 import Subtitle from "./Shared/Subtitle.vue";
 import Container from "./Shared/Container.vue";
+import { data } from "@/assets/data/dummyData";
 
 export default {
   name: "OurTeam",
@@ -22,6 +39,11 @@ export default {
     SectionTitle,
     Container,
     Subtitle,
+  },
+  data() {
+    return {
+      team: data.team,
+    };
   },
 };
 </script>
